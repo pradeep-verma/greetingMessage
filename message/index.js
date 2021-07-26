d1590132295dc6a7729d3596a9571acfa531e58f
pradeep verma <pradeep.verma.5dec@gmail.com>
2021-07-26 17:34:20 +0000

    greetingMessage doubt

---
+++ message/index.js
@@ -0,0 +1,4 @@
+const greetingMessage = require("/home/workspace/nodejs/coding-practices/coding-practice-2b/greeting/index");
+let message = "Hello Rahul! " + greetingMessage;
+console.log(message);
+module.exports = message;
