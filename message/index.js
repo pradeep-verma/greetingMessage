ad353f3ba0649380131f8f3b65d5c9098fb17102
pradeep verma <pradeep.verma.5dec@gmail.com>
2021-07-27 06:01:59 +0000

    exporting templete

--- message/index.js
+++ message/index.js
@@ -1,4 +1,2 @@
 const greetingMessage = require("/home/workspace/nodejs/coding-practices/coding-practice-2b/greeting/index");
-let message = "Hello Rahul! " + greetingMessage;
-console.log(message);
-module.exports = message;
+module.exports = "Hello Rahul! " + greetingMessage;
